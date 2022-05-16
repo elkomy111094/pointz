6ce18d4dcfb6007370c7eeefaa66c3af9e11ed3e
eslam elkomy <elkomy.dev@gmail.com>
2022-05-16 02:54:41 +0200

    OrderStepperScreen Commit

---
+++ lib/Views/Screens/orders_stepper_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OrdersStepperScreen extends StatefulWidget {
+  @override
+  _OrdersStepperScreenState createState() => _OrdersStepperScreenState();
+}
+
+class _OrdersStepperScreenState extends State<OrdersStepperScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
